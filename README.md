dc20481017459d4b82c3c4cc2f132d493a5fda3d
Brandon Fowler <brandfowl@gmail.com>
2024-03-17 17:19:29 -0400

    Improve documentation

--- README.md
+++ README.md
@@ -1,18 +1,43 @@
-# System
+# system
 
-Trait for the `Command` struct to conveniently run system commands, like the C system function.
+Crate to easily run system/shell commands across different platforms, similar to the C `system` command.
 
-## Example
+## Usage
+
+### `system`
+
+For simple use cases where you just need the output of a system command, the `system` can be used.
 
 ```rust
-use std::process::Command;
-
-// Import System trait to use Command::system
-use system::System;
+use system::system;
 
 fn main() {
-    let _ = Command::system("echo Hello, world! && echo 123").status();
+    let out = system("echo Hello, world!").expect("Failed to run command.");
+
+    #[cfg(target_os = "windows")]
+    assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\r\n");
+
+    #[cfg(not(target_os = "windows"))]
+    assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\n");
 }
 ```
 
-Prints `Hello, world!` and `123`.
+### `std::process::Command::system`
+
+For more complex uses cases where the underlying `Command` has to be modified before running the command, the `system::System` trait is provided for `Command`.
+
+The trait adds the function `Command::system` to create `Command`s that execute system/shell commands.
+
+For example,
+
+```rust
+    let out = Command::system("echo Hello, world!")
+        .output()
+        .expect("Failed to run command.");
+
+    #[cfg(target_os = "windows")]
+    assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\r\n");
+
+    #[cfg(not(target_os = "windows"))]
+    assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\n");
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
-use std::process::Command;
+//! Crate to easily run system/shell commands across different platforms, similar to the C `system` command.
+
+use std::{
+    io,
+    process::{Command, Output},
+};
 
 #[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
 
+/// Trait to allow for the creation of system commands.
 pub trait System<T> {
     fn system(command: &str) -> T;
 }
 
 impl System<Command> for Command {
     #[cfg(target_os = "windows")]
+    /// Construct a new [Command] that runs the given system command when executed.
+    ///
+    /// See [Command::new] for the default configuration.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use std::process::Command;
+    ///
+    /// use system::System;
+    ///
+    /// fn main() {
+    ///     let out = Command::system("echo Hello, world!")
+    ///         .output()
+    ///         .expect("Failed to run command.");
+    ///
+    ///     #[cfg(target_os = "windows")]
+    ///     assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\r\n");
+    ///
+    ///     #[cfg(not(target_os = "windows"))]
+    ///     assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\n");
+    /// }
+    /// ```
     fn system(command: &str) -> Command {
         let mut rust_command = Command::new("cmd");
         rust_command.arg("/c");
@@ -17,6 +46,29 @@ impl System<Command> for Command {
     }
 
     #[cfg(not(target_os = "windows"))]
+    /// Construct a new [Command] that runs the given system command when executed.
+    ///
+    /// See [Command::new] for the default configuration.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use std::process::Command;
+    ///
+    /// use system::System;
+    ///
+    /// fn main() {
+    ///     let out = Command::system("echo Hello, world!")
+    ///         .output()
+    ///         .expect("Failed to run command.");
+    ///
+    ///     #[cfg(target_os = "windows")]
+    ///     assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\r\n");
+    ///
+    ///     #[cfg(not(target_os = "windows"))]
+    ///     assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\n");
+    /// }
+    /// ```
     fn system(command: &str) -> Command {
         let mut rust_command = Command::new("sh");
         rust_command.arg("-c");
@@ -25,6 +77,21 @@ impl System<Command> for Command {
     }
 }
 
+/// Run a system/shell command and return the [Output].
+///
+/// # Example
+/// use system::system;
+///
+/// ```rust
+/// fn main() {
+///     let out = system("echo Hello, world!").expect("Failed to run command.");
+///
+///     #[cfg(target_os = "windows")]
+///     assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\r\n");
+///
+///     #[cfg(not(target_os = "windows"))]
+///     assert_eq!(String::from_utf8_lossy(&out.stdout), "Hello, world!\n");
+/// ```
 pub fn system(command: &str) -> io::Result<Output> {
     Command::system(command).output()
 }
